257fd08ba5ef3bfb604d6fcd0afdf9b28219c554
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-04-24 00:44:36 +0900

    Create aws_lambda.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,3 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
-
-resource "aws_dynamodb_table" "kindle_sale_notificator" {
-  name = "kindle_sale_notificator"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "id"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-  attribute {
-    name = "price"
-    type = "N"
-  }
-  attribute {
-    name = "point"
-    type = "N"
-  }
-  attribute {
-    name = "last_update_date"
-    type = "S"
-  }
-  attribute {
-    name = "last_update_time"
-    type = "S"
-  }
-}
