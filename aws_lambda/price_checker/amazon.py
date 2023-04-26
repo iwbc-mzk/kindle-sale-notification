81c7923fbffa79447ab37df028817f336f322450
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-04-26 22:49:11 +0900

    スクレイピング用lambdaの追加

---
+++ aws_lambda/price_checker/amazon.py
@@ -0,0 +1,62 @@
+import re
+from urllib import robotparser, parse
+
+from bs4 import BeautifulSoup
+import requests
+
+class AmazonScraper:
+    def __init__(self) -> None:
+        self._base_url = "https://www.amazon.co.jp"
+        self._user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+        self._headers = {
+            "User-Agent": self._user_agent,
+            "Accept-Language": "ja,en-US;q=0.9,en;q=0.8"
+        }
+        self._prepare_robots()
+
+        self._swatch_selector = "#tmmSwatches > ul > li.swatchElement"
+
+    def _prepare_robots(self):
+        self._robots = robotparser.RobotFileParser()
+        robots_url = parse.urljoin(self._base_url, "robots.txt")
+        self._robots.set_url(robots_url)
+        self._robots.read()
+
+    def fetch_html(self, item_id: str) -> None:
+        url = parse.urljoin(self._base_url, f"dp/{item_id}")
+        if not self._robots.can_fetch(self._user_agent, url):
+            raise ConnectionRefusedError(f"This url is not allowed to requests. [{url}]")
+        
+        res = requests.get(url, headers=self._headers)
+        if res.ok:
+            self._soup = BeautifulSoup(res.content, "html.parser")
+        else:
+            res.raise_for_status()
+
+    def get_price(self) -> int:
+        swatch_elements = self._soup.select(self._swatch_selector)
+        for ele in swatch_elements[0].find_all("span"):
+            text = ele.text
+            yen_unit = "￥"
+
+            if yen_unit in text:
+                re_yen = f"{yen_unit}[0-9,]*"
+                price = int(re.search(re_yen, text)[0].replace(yen_unit, "").replace(",", ""))
+        
+        return price
+
+    def get_point(self) -> int:
+        swatch_elements = self._soup.select(self._swatch_selector)
+        for ele in swatch_elements[0].find_all("span"):
+            text = ele.text
+            pt_unit = "pt"
+
+            if pt_unit in text:
+                re_pt = f"[0-9,]*{pt_unit}"
+                pt = int(re.search(re_pt, text)[0].replace(pt_unit, "").replace(",", ""))
+        
+        return pt
+
+    def get_title(self) -> str:
+        title_element = self._soup.find(id="productTitle")
+        return title_element.text if title_element else ""
---
+++ aws_lambda/price_checker/price_checker.py
@@ -0,0 +1,67 @@
+import json
+import os
+from datetime import datetime
+from urllib.request import HTTPError
+
+from amazon import AmazonScraper
+import boto3
+
+
+def lambda_handler(event, context):
+    sqs = boto3.resource("sqs")
+    queue = sqs.Queue(os.environ["queue_url"])
+
+    messages = queue.receive_messages(MaxNumberOfMessages=1)
+    if not messages:
+        print("No messages.")
+        return
+
+    message = messages[0]
+    item = json.loads(message.body)
+    id = item.get("id")
+    if not id:
+        print("No item id.")
+        return
+
+    scraper = AmazonScraper()
+    try:
+        scraper.fetch_html(id)
+    except HTTPError as e:
+        print(e)
+        return
+
+    values = {
+        "title": scraper.get_title(),
+        "price": scraper.get_price(),
+        "point": scraper.get_point(),
+    }
+
+    need_update = False
+    updated_item = item.copy()
+    for key, val in values.items():
+        if item.get(key) != val:
+            updated_item[key] = val
+            need_update = True
+
+    # 前回より100円以上安くなった場合
+    is_discounted = (item.get("price") - item.get("point")) - \
+        (updated_item.get("price") - updated_item.get("point")) >= 100
+    updated_item["discounted"] = "Y" if is_discounted else "N"
+    need_update |= is_discounted | is_discounted is not item.get("discounted")
+
+    updated_item["updated_at"] = int(datetime.now().timestamp())
+
+    try:
+        if need_update:
+            dynamodb = boto3.resource("dynamodb")
+            dynamodb_table = dynamodb.Table(os.environ["table_name"])
+            dynamodb_table.put_item(Item=updated_item)
+    except Exception as e:
+        print(e)
+    else:
+        queue.delete_messages(Entries=[
+            {
+                "Id": message.message_id,
+                "ReceiptHandle": message.receipt_handle
+            }
+        ])
