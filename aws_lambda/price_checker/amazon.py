02e6fa793eff143c4f14e9aa5ba684f3c65ccb15
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-04-28 17:37:32 +0900

    リクエスト失敗時にユーザーエージェントを変更して再実行するように修正

--- aws_lambda/price_checker/amazon.py
+++ aws_lambda/price_checker/amazon.py
@@ -7,9 +7,13 @@ import requests
 class AmazonScraper:
     def __init__(self) -> None:
         self._base_url = "https://www.amazon.co.jp"
-        self._user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+        self._user_agents = [
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763",
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) CriOS/31.0.1650.18 Mobile/11B554a Safari/8536.25"
+        ]
         self._headers = {
-            "User-Agent": self._user_agent,
+            "User-Agent": self._user_agents[0],
             "Accept-Language": "ja,en-US;q=0.9,en;q=0.8"
         }
         self._prepare_robots()
@@ -24,14 +28,26 @@ class AmazonScraper:
 
     def fetch_html(self, item_id: str) -> None:
         url = self.get_url(item_id)
-        if not self._robots.can_fetch(self._user_agent, url):
-            raise ConnectionRefusedError(f"This url is not allowed to requests. [{url}]")
-        
-        res = requests.get(url, headers=self._headers)
-        if res.ok:
-            self._soup = BeautifulSoup(res.content, "html.parser")
-        else:
-            res.raise_for_status()
+        max_retry = 3
+        for retry in range(max_retry):
+            user_agent = self._user_agents[retry]
+            if not self._robots.can_fetch(user_agent, url):
+                raise ConnectionRefusedError(f"This url is not allowed to requests. [{url}]")
+            
+            headers = {
+                **self._headers,
+                "User-Agent": user_agent
+            }
+            res = requests.get(url, headers)
+            if res.ok:
+                print(f"successed to requet: user_agent={user_agent} url={url}")
+                self._soup = BeautifulSoup(res.content, "html.parser")
+                break
+            else:
+                if retry < max_retry:
+                    print(f"failed to requet: user_agent={user_agent} url={url}")
+                    continue
+                res.raise_for_status()
 
     def get_price(self) -> int:
         swatch_elements = self._soup.select(self._swatch_selector)
@@ -59,7 +75,7 @@ class AmazonScraper:
 
     def get_title(self) -> str:
         title_element = self._soup.find(id="productTitle")
-        return title_element.text.strip() if title_element else ""
+        return title_element.text if title_element else ""
     
     def get_url(self, item_id: str) -> str:
         return parse.urljoin(self._base_url, f"dp/{item_id}")
