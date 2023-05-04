165812bba8e56265e544f86aef6837778c8dff54
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-05-04 17:44:33 +0900

    terraform destroy実行時にECRにイメージが残っていてもECRを削除するように変更

--- terraform/aws_ecr.tf
+++ terraform/aws_ecr.tf
@@ -4,6 +4,8 @@
 resource "aws_ecr_repository" "price_checker" {
   name                 = "price_checker"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
