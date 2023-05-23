d11896c4acee3b8aba0cf164dabef6d475dd7497
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-05-23 21:59:27 +0900

    kindleページ以外に登録ボタンが表示されていた問題の修正

--- chrome_extension/react-chrome-app/src/content/index.tsx
+++ chrome_extension/react-chrome-app/src/content/index.tsx
@@ -5,18 +5,20 @@ import App from './content';
 import { isKindlePage } from './urils';
 import { MESSAGE_TYPES } from '../const';
 
-const rootElement = document.createElement('div');
-rootElement.id = 'register-button';
+if (isKindlePage()) {
+    const rootElement = document.createElement('div');
+    rootElement.id = 'register-button';
 
-const ele = document.getElementById('wishlistButtonStack');
-ele?.appendChild(rootElement);
+    const ele = document.getElementById('wishlistButtonStack');
+    ele?.appendChild(rootElement);
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
 
 chrome.runtime.sendMessage({
     type: MESSAGE_TYPES.ChangeIconStateMessage,
